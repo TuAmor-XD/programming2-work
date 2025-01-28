1dd25b2eaf2e315c748bebcd2b6a897c294d5f72
TuAmor-Alfred <2021256010@ub.edu.bz>
2025-01-27 21:24:53 -0600

    New commit

--- nose2 xd.cpp
+++ nose2 xd.cpp
@@ -2,13 +2,24 @@
 using namespace std;
 
 void multiplynumber(int[], int);
+void printReverse(int[], int);
 
 int main()
 {
     int arrsize = 5;
     int a[arrsize] = {2,4,6,8,10};
     multiplynumber(a, arrsize);
+    cout << endl;
+    printReverse(a, arrsize);
     return 0;
+}
+
+void printReverse(int f[], int size)
+{
+    for(int i = size - 1; i >= 0; i--)
+    {
+        cout << f[i] << " ";
+    }
 }
 
 // multiply each by 2
@@ -20,5 +31,4 @@ void multiplynumber(int b[], int size)
         b[i] = b[i] * 2;
         cout << b[i] << " ";
     }
-
 }

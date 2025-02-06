5e59533ad34827116f33a0bfe261c06968d9f291
Alfred Brian Acosta <113042307+TuAmor-XD@users.noreply.github.com>
2025-02-05 18:18:41 -0600

    class commit

--- week2/function2.cpp
+++ week2/function2.cpp
@@ -1,59 +1,27 @@
 #include<iostream>
 using namespace std;
 
-void averageOfNumbers(int [], int);
+int averageOfNumbers(int [], int);
 
 int main()
 {
     int size = 2;
-    int x[size] = {};
+    int x[size] = {8, 4};
     
-    averageOfNumbers(x, size);
+    int return = averageOfNumbers(x, size);
+    cout << return;
 
     return 0;
 }
 
-void averageOfNumbers(int a[], int size)
+int averageOfNumbers(int a[], int size)
 {
-    int sum = 0;
+
     int average = 0;
     for(int i = 0; i < size; i++)
     {
-        cout << "Enter 2 numbers: ";
-        cin >> a[i];
+        average /= a[i];
     }
-
-    for(int j = 0; j < size; j++)
-    {
-        cout << "You have enterned: " << a[j] << " ";
-    }
-
-    cout << endl;
-
-    for(int x = 0; x < size; x++)
-    {
-        sum += a[x];    
-    }
-    cout << endl;
-    cout << "Sum of all numbers are: " << sum << ".\n";
-
-
-    int choice;
-    cout << "You you like to find the average? \n";
-    cout << "*****Enter 1 for yes and -1 for no****\n";
-    cin >> choice; 
-
-        if(choice == 1)
-        {
-            average = sum / size;
-            cout << "The average is: " << average << ".\n";
-            
-        }else if(choice == -1)
-        {
-            cout << "Well byeeeee :v \n";
-        }
-        
-
-    
+        return average;
 
 }
--- week2/function8.cpp
+++ week2/function8.cpp
@@ -1,1 +1,8 @@
 //8.	Search for an Element: Write a function that searches for a given element and returns its index (or -1 if not found).
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}

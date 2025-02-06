fa5b8fbf01f5164f93f31a950f59b6267e115900
Alfred Brian Acosta <113042307+TuAmor-XD@users.noreply.github.com>
2025-02-05 18:40:28 -0600

    xdd

--- week2/function3.cpp
+++ week2/function3.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
 using namespace std;
 
+int sumOfNumbers(int [], int);
 int maxXd(int [], int);
+int averageOfNumbers(int [], int);
+int smallestNum(int [], int);
+void reverseXd(int [], int);
+void evenXd(int [], int);
+void oddXd(int [], int);
 
 int main()
 {
     int size = 10;
     int x[size] = {1,2,3,4,5,6,7,8,9,10};
-
+    
     int result = maxXd(x, size);
     cout << "The maximum number in the array is " << result << ".\n";
 
--- week2/function9.cpp
+++ week2/function9.cpp
@@ -1,1 +1,30 @@
 //9.	Replace Element: Write a function that replaces all occurrences of a given value with another value.
+#include<iostream>
+using namespace std;
+
+int replace(int [], int, int);
+
+int main()
+{
+    int size = 10; 
+    int xd[size] = {2,4,6,8,10,12,14,16,18,20};
+
+    int result = replace(xd, size, '404');
+    cout << result;
+
+    return 0;
+}
+
+int replace(int a[], int size, int rep)
+{
+        int result;
+        int replace = 0;
+        for(int i = 0; i < size; i++)
+        {
+            if(a[i] == 4 * 2)
+            {
+                a[i] == replace;
+            }
+        }
+        return result; 
+}

696b32b26e53901a1c347281dfee3163585bb369
Alfred Brian Acosta <113042307+TuAmor-XD@users.noreply.github.com>
2025-02-05 18:05:15 -0600

    Update function1.cpp

--- week2/function1.cpp
+++ week2/function1.cpp
@@ -1,38 +1,32 @@
 #include<iostream>
 using namespace std;
 
-void sumOfNumbers(int [], int);
+int sumOfNumbers(int [], int);
 
 int main()
 {
     int size = 2;
     int x[size] = {};
+    int num;
+    for(int i = 0; i < size; i++)
+    {
+        cout << "Enter 2 numbers: ";
+        cin >> num[i];
+    }
     
-    sumOfNumbers(x, size);
+    int result = sumOfNumbers(x, size);
+    cout << result << endl;
 
     return 0;
 }
 
-void sumOfNumbers(int a[], int size)
+int sumOfNumbers(int a[], int size)
 {
     int sum = 0;
-    for(int i = 0; i < size; i++)
-    {
-        cout << "Enter 2 numbers: ";
-        cin >> a[i];
-    }
-
-    for(int j = 0; j < size; j++)
-    {
-        cout << "You have enterned: " << a[j] << " ";
-    }
-
-    cout << endl;
 
     for(int x = 0; x < size; x++)
     {
         sum += a[x];    
     }
-
-    cout << "Sum of all numbers are: " << sum << ".\n";
+    return sum;
 }

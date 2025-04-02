95da70dd3224763783287c20bef34a735211aa1e
TuAmor-Alfred <2021256010@ub.edu.bz>
2025-04-01 22:36:47 -0600

    xdd

--- week8/class.cpp
+++ week8/class.cpp
@@ -4,60 +4,117 @@ using namespace std;
 
 class Node {
 public:
-    string name;  // Name of the student
+    string name;  
+    int age;      
     Node* next;   // Pointer to the next node (student in line)
 
-    // Constructor to initialize node with a student's name
-    Node(string studentName);
+    // Constructor to initialize node with a student's name and age
+    Node(string studentName, int studentAge);
 
-    // Function to display the node details (name and address)
+    // Function to display the node details (name, age, and address)
     void display() const;
     
     // Function to print all the nodes in the list
-    void printList(Node* head);
-    // Function to print all the nodes in the list
+    void printList(Node* head) const;
+
+    // Function to count the number of nodes in the list
+    int countList(Node* head) const;
+
+    void printAbove17(Node* head) const;
+
+    bool search(Node* head, const string& targetName) const;  // Added targetName parameter
 };
 
-// Constructor to initialize node with a student's name
-Node::Node(string studentName) {
+// Constructor to initialize node with a student's name and age
+Node::Node(string studentName, int studentAge) {
     name = studentName;
+    age = studentAge;
     next = nullptr;  // Initially, the next pointer is null (end of the list)
 }
 
-// Function to display the node details (name and address)
+// Function to display the node details (name, age, and address)
 void Node::display() const {
-    cout << "Student Name: " << name << ", Memory Address: " << this << endl;
+    cout << "Student Name: " << name << ", Student Age: " << age << ", Memory Address: " << this << endl;
 }
 
-// Function to print all the nodes in the linked list
-void Node::printList(Node* head) {
+void Node::printList(Node* head) const {
     Node* current = head;
-    while (current != nullptr) {
-        current->display();  // Display the current node
+    while (current != nullptr) { 
+        current->display(); 
         current = current->next;  // Move to the next node
     }
 }
 
+int Node::countList(Node* head) const {
+    int count = 0;  // Initialize
+    Node* current = head;  // Start from the head
+    while (current != nullptr) { 
+        count++;  // Increment count
+        current = current->next;  // Move to the next node
+    }
+    return count;  // Return the total count
+}
+
+void Node::printAbove17(Node* head) const {
+    Node* current = head;
+    while(current != nullptr) {
+        if(current->age > 17) { // if the current age is more than 17
+           cout << current->name << endl; // we print out the name of the student
+        }
+        current = current->next; // move to next node
+    }
+}
+
+bool Node::search(Node* head, const string& targetName) const {
+    Node* current = head;
+    while(current != nullptr) {
+        if(current->name == targetName) {  // Check if current node matches target
+            return true;  // Found the student
+        }
+        current = current->next;  // Move to next node
+    }
+    return false;  // Student not found in list
+}
 
 int main() {
-    // Create nodes (students) without using `new`
-    Node student1("Alice");
-    Node student2("Mary");
-    Node student3("John");
+    // Create nodes (students) using the correct constructor
+    Node student1("Alice", 18);
+    Node student2("Mary", 15);
+    Node student3("John", 25);
     
+    // Link the nodes
     student1.next = &student2;
-    cout << "the queue looks like tihis: ";
+    student2.next = &student3;
+
+    // Print the list starting from student1
+    cout << "The queue looks like this: " << endl;
     student1.printList(&student1);
     cout << endl;
 
+    // Count the number of students in the list
+    cout << "The number of students in the line is: " << student1.countList(&student1) << endl;
 
-    student2.next = &student3;
-    cout << "the queue looks like tihis: ";
-    student2.printList(&student2);
+    // Create another student
+    Node student4("David", 20);
+    student3.next = &student4;  // Link student3 to student4
+
+    // Print the updated list
+    cout << "The updated queue looks like this: " << endl;
+    student1.printList(&student1);
     cout << endl;
 
-    Node student4("David");
+    // Count the number of students again
+    cout << "The number of students in the line after adding David is: " << student1.countList(&student1) << endl;
+
+    cout << "The people above age 17 are: " << endl;
+    student1.printAbove17(&student1);
+
+    // Test search function
+    bool result = student1.search(&student1, "Nancy");
+    cout << "Search for Nancy: " << (result ? "Found" : "Not found") << endl;
     
+    result = student1.search(&student1, "John");
+    cout << "Search for John: " << (result ? "Found" : "Not found") << endl;
 
     return 0;
 }
---
+++ week8/main3.cpp
@@ -0,0 +1,146 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Node 
+{
+public:
+    string name;
+    int age;
+    Node* next;
+
+    Node(string studentName, int studentAge);
+
+    void display() const;
+
+    void printList(Node* head) const;
+
+    int countList(Node* head) const;
+
+    void printAbove20(Node* head) const;
+    
+    bool search(Node* Head, const string &targetName) const;
+
+};
+
+
+
+Node::Node(string studentName, int studentAge)
+{
+    name = studentName;
+    age = studentAge;
+    next = nullptr;
+}
+
+void Node::display() const
+{
+    cout << "Student name: " << name << ", Student age: " << age << ", Memory adress" << this << endl;
+}
+
+void Node::printList(Node* head) const
+{
+    Node* current = head;
+    while(current != nullptr)
+    {
+        current->display();
+        current = current->next;
+    }
+}
+
+// int Node::countList(Node* head) const
+// {
+//     int count = 0;
+//     Node* current = head;
+//     while(current != nullptr)
+//     {
+//         count++;
+//         current = current->next;
+//     }
+//     return count;
+// }
+
+int Node :: countList(Node* head) const
+{
+    int count = 0;
+    Node* current = head;
+    while(current != nullptr)
+    {
+        count++;
+        current = current->next;
+    }
+    return count;
+}
+
+void Node::printAbove20(Node* head) const
+{
+    int agex = 20;
+    Node* current = head;
+    while(current != nullptr)
+    {
+        if(current-> age > agex)
+        {
+            cout << current->name << endl;
+        }
+        current = current->next;
+    }
+}
+
+bool Node::search(Node* head, const string& targetName) const 
+{
+    Node* current = head;
+    while(current != nullptr) 
+    {
+        if(current->name == targetName) 
+        {
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
+
+int main()
+{
+    Node student1("Alfred", 19);
+    Node student2("Gillroy", 17);
+    Node student3("Akeem", 30);
+    
+
+    student1.next = &student2;
+    student2.next = &student3;
+    
+    cout << "The queue looks like this: " << endl;
+    student1.printList(&student1);
+    cout << endl;
+
+    cout << "The number of students int the line is: " << student1.countList(&student1) << endl;
+
+    Node student4("Cesar", 22);
+    student3.next = &student4;
+    cout << "The updated queue looks like this: " << endl;
+    student1.printList(&student1);
+    cout << endl;
+
+    cout << "The number of students int the line is: " << student1.countList(&student1) << endl;
+
+    cout << "The students above age 20 are: " << endl;
+    student1.printAbove20(&student1);
+
+    Node student5("Dennis", 27);
+    student4.next = &student5;
+    student1.printList(&student1);
+    cout << endl;
+
+    cout << "The queue looks like this: " << endl;
+    student1.printList(&student1);
+    cout << endl;
+
+    cout << "The number of students int the line is: " << student1.countList(&student1) << endl;
+
+    cout << "Search results:" << endl;
+    cout << "Is Akeem in the list? " << (student1.search(&student1, "Akeem") ? "Yes" : "No") << endl;
+    cout << "Is Mary in the list? " << (student1.search(&student1, "Mary") ? "Yes" : "No") << endl;
+
+    return 0;
+
+}
